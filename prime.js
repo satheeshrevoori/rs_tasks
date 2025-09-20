8304640b8b045750892fe5a7a2f6aac9aeadf8c8
Satheesh Revoori <104457018+satheeshrevoori@users.noreply.github.com>
2025-09-20 17:18:29 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,14 +1,23 @@
 function isPrime(n) {
   // Corner case
   if (n <= 1)
+  {
+    
     return false;
+  }
 
   // Check from 2 to n-1
-  for (let i = 2; i < n; i++)
+  else {
+  for (let i = 2; i < n; i++){
+    
+    
     if (n % i == 0)
+    {
       return false;
-
+    }
   return true;
+  }
+}
 }
 
 // Driver Code
